56c626816c23001dc48c05c2bbcf3bc427010467
Sachin Jha <sjhahere@gmail.com>
2024-07-12 05:23:56 +0530

    Use redux-persist to store values

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todoSlice from "../slices/todoSlice";
+import todoReducer from "../slices/todoSlice";
+import storage from "redux-persist/lib/storage";
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedTodoReducer = persistReducer(persistConfig, todoReducer)
 
 const store = configureStore({
     reducer: {
-        todo: todoSlice
-    }
+        todo: persistedTodoReducer
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 })
 
+export const persistor = persistStore(store)
 export default store;
 export type RootState = ReturnType<typeof store.getState>
